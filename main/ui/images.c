83e437202fb47cf0d380da31c0675b08b354048a
davidrfloydii <davidrfloydii@gmail.com>
2026-07-23 20:28:08 -0500

    Moving to larger screen

--- main/ui/images.c
+++
@@ -1,8 +0,0 @@
-#include "images.h"
-
-const ext_img_desc_t images[4] = {
-    { "trailer_back", &img_trailer_back },
-    { "trailer_side", &img_trailer_side },
-    { "ImgChevronUp32px", &img_img_chevron_up32px },
-    { "water_level_bg", &img_water_level_bg },
-};
